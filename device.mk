b037380b8d2912c3a7bbf2091446044202b9629a
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-11 08:40:01 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ TW_EXCLUDE_TWRPAPP := true
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
+#Properties
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
+
 RECOVERY_BINARY_SOURCE_FILES += \
     $(TARGET_OUT_VENDOR_EXECUTABLES)/hw/vendor.qti.hardware.vibrator.service
 
